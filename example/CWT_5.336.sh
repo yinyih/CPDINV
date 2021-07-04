55f99842fb4822be3aeb6da363e3d90f83aa6796
yinyih <44935058+yinyih@users.noreply.github.com>
2021-07-04 13:52:24 +0800

    Add files via upload

---
+++ example/CWT_5.336.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#Calculate the Zb using a center wavenumber of 5.336
+
+Proj ESA.xyz 1 125 20 mag.dat
+Minc mag.dat -2875.06055/2875.06055/-3429.12256/3633.48022 2.8 2.8 mag_test.grd
+Cwt mag_test.grd 5.336 4.0 10 spectra_cwt_5.336.dat 
+Lsfit spectra_cwt_5.336.dat Zt 3 43 32 37 Zt_5.336_dat Zt_5.336_uncertainty.dat
+Lsfit spectra_cwt_5.336.dat Z0 3 43 22 31 Z0_5.336_dat  Z0_5.336_uncertainty.dat
+Depth Z0_5.336.dat Zt_5.336.dat Z0_5.336_uncertainty.dat Zt_5.336_uncertainty.dat Zb_5.336.dat Zb_5.336_uncertainty.dat
+Proj Zb_5.336_uncertainty.dat -1 125 20 Z_5.336_uncertainty.dat
+
+xyzname=Z_5.336.dat
+grdname=Z_5.336.grd
+cptname=Z_cwt_average.cpt
+filename=Z_5.336.ps
+toponame=topo.grd
+
+R=100/150/-10/50
+
+
+gmt surface $xyzname -G$grdname -R$R -I0.05/0.05
+gmt surface topo.xyz -G$toponame -R$R -I0.05/0.05
+gmt grdgradient $toponame -Ne0.7 -A90 -G$toponame.light
+gmt grdimage -R$R -Ba20WSEN -JU14c $grdname -C$cptname -E300 -K -V -I$toponame.light >$filename
+gmt psmask NaN.xyz -R$R -JU14c -I0.05/0.05 -Ggrey -K -O -V >> $filename
+gmt psmask -C -K -O >>$filename
+gmt pscoast -R$R  -JU14c -B10/10 -O -K -Dh -Ggrey -A20000 -I1 -W1/0.01i -N3/1p >>$filename
+gmt psscale -D3i/-0.3i/4i/0.1ih -O -E  -I -C$cptname -Ba5/:"(km)": >>$filename
+
---
+++ example/FFT_300.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#Calculate the Zb using a window size of 302.4km
+
+Proj ESA.xyz 1 125 20 mag.dat
+Minc mag.dat -2875.06055/2875.06055/-3429.12256/3633.48022 2.8 2.8 mag_test.grd
+Wft mag_test.grd 116 36 0.006 0.15 spectra_fft_300.dat 
+Lsfit spectra_fft_300.dat Zt 3 27 7 14 Zt_300.dat Zt_300_uncertainty.dat
+Lsfit spectra_fft_300.dat Z0 3 27 1 6 Z0_300.dat  Z0_300_uncertainty.dat
+Depth Z0_300.dat Zt_300.dat Z0_300_uncertainty.dat Zt_300_uncertainty.dat Zb_300.dat Zb_300_uncertainty.dat
+Proj Zb_300_uncertainty.dat -1 125 20 Z_300_uncertainty.dat
+
+xyzname=Z_300.dat
+grdname=Z_300.grd
+cptname=Z_fft_average.cpt
+filename=Z_300.ps
+toponame=topo.grd
+
+R=100/150/-10/50
+
+
+gmt surface $xyzname -G$grdname -R100/150/-10/50 -I0.05/0.05
+gmt surface topo.xyz -G$toponame -R100/150/-10/50 -I0.05/0.05
+gmt grdgradient $toponame -Ne0.7 -A90 -G$toponame.light
+gmt grdimage -R$R -Ba10WSEN -JU14c $grdname -C$cptname -E300 -K -V -I$toponame.light >$filename
+gmt psmask NaN.xyz -R$R -JU14c -I0.05/0.05 -Ggrey -K -O -V >> $filename
+gmt psmask -C -K -O >>$filename
+gmt pscoast -R$R  -JU14c -B10/10 -O -K -Dh -Ggrey -A30000 -I1 -W1/0.01i -N3/1p >>$filename
+gmt psscale -D3i/-0.3i/4i/0.1ih -O -E  -I -C$cptname -Ba5/:"(km)": >>$filename
+
